911b3b1ff9119b4eab011f9427b86b70d6c34fe1
ramhawks <winonaramhawks@gmail.com>
2026-02-24 11:39:01 -0600

    Update motor controller IDs in FuelConstants
    Swapped motor controller IDs for feeder and intake launcher motors.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,8 +42,8 @@ public final class Constants {
 
   public static final class FuelConstants {
     // Motor controller IDs for Fuel Mechanism motors
-    public static final int FEEDER_MOTOR_ID = 19;
-    public static final int INTAKE_LAUNCHER_MOTOR_ID = 18;
+    public static final int FEEDER_MOTOR_ID = 18;
+    public static final int INTAKE_LAUNCHER_MOTOR_ID = 19;
 
     // Current limit and nominal voltage for fuel mechanism motors.
     public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
